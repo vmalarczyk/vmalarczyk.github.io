78eb212e7e26301e252314be2213b2896d7e032e
Victoria M. Malarczyk <malarczyk@princeton.edu>
2025-07-28 16:59:20 +0000

    Update website features

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -2,12 +2,11 @@ const sentences = [
   "I am currently a PhD Candidate at <span style='color: #e2725b;'><strong>Princeton University</strong></span> in experimental fluid mechanics. I work with Prof. Marcus Hultmark in the <span style='color: #3aa6b9;'><strong>Fundamental and Applied Studies in Turbulence (FAST) lab</strong></span>.",
   'My accomplishments include experimentally achieving some of the highest Reynolds number airfoil aerodynamic data (exceeding 10 million) by working with a variable pressure wind tunnel.',
   "I earned my BS in Aerospace Engineering from the <span style='color: #f5d76e;'><strong>University of Southern California</strong></span>, and hold an MA in Mechanical Engineering from <span style='color: #e2725b;'><strong>Princeton University</strong></span>.",
-  "I have previously worked in the aerospace industry supporting projects for <span style='color: #3a69b9ff;'><strong>Rolls-Royce</strong></span> and neurobiology research at <span style='color: #b93a43ff;'><strong>Harvard University</strong></span>.",
   'I am also passionate about inclusive teaching and making engineering education accessible to everyone. Have a look around!',
 ];
 
 const typingSpeed = 10;
-const pauseBetweenSentences = 250;
+const pauseBetweenSentences = 100;
 
 const container = document.getElementById('typingText');
 
